c877c3856e7ce07740d5833337065388ab7460f0
Jainam Parikh <jainamparikh0825@gmail.com>
2022-12-20 22:05:16 +0530

    update: singly linked list and its length

--- LinkedList/singlyLL.cpp
+++ LinkedList/singlyLL.cpp
@@ -27,12 +27,16 @@ int main(){
     second->next = third;
     third->next = fourth;
 
+    int count = 0;
     Node *temp = head;
     while(temp->next != NULL){
+        count++;
         cout << temp->data << " ";
         temp = temp->next;
     }
     cout << temp->data << endl;
+    ++count;
+    cout << "Length: " << count << endl;
 
     return 0;
 }
